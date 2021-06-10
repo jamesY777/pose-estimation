83b96ac87e544a277745d87e34732e9ebafbc377
jamesY777 <82723087+jamesY777@users.noreply.github.com>
2021-06-09 22:24:32 -0400

    start with minimum requirement of the pose capture

---
+++ PoseEstimationMin.py
@@ -0,0 +1,32 @@
+import cv2
+import mediapipe as mp
+import time
+
+cap = cv2.VideoCapture(0) #REad the video'Videos/video2.mp4'
+pTime = 0
+
+mpPose = mp.solutions.pose
+pose = mpPose.Pose() #Default static_image_mode=False; this is to detect and track the Pose
+mpDraw = mp.solutions.drawing_utils #Use this to draw the 
+
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) #The default is BGR for video capture, convert to RGB as mediapipe using RGB
+    results = pose.process(imgRGB)
+
+    if results.pose_landmarks: #Pose landmarks are being divided into 32 dots
+        mpDraw.draw_landmarks(img, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
+        for id, lm in enumerate(results.pose_landmarks.landmark):
+            h, w, c = img.shape #Get the height, width of the image capture
+            cx, cy = int(lm.x*w), int(lm.y*h) #Calculate the normolized position to pixel
+            cv2.circle(img, (cx,cy), 5, (255,0,0), cv2.FILLED)
+            print(id, cx, cy)
+    #Create the FPS tracking
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+    cv2.putText(img, str(int(fps)), (70,50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+    
+    
+    cv2.imshow('Image', img) #Display the video
+    cv2.waitKey(1)
---
+++ PoseModule.py
@@ -0,0 +1,65 @@
+"""
+James Yan - June 09, 2021 
+Creating a class that can be reusable from other project quickly that using the Post track Tech.
+The class will creat mpPose object that can process a image or stream image and detect the Pose (body) from the image.
+The class wiil create mp.Draw object which will draw the dots/connection on the detected Pose within the images.
+"""
+
+import cv2
+import mediapipe as mp
+import time
+
+class poseDetector():
+    def __init__(self, mode=False, complexity=1, smooth=True, detectionCon=0.5, trackCon=0.5):
+        self.mode = mode
+        self.complexity = complexity
+        self.smooth = smooth
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+        self.mpPose = mp.solutions.pose
+        self.pose = self.mpPose.Pose(self.mode, self.complexity, self.smooth, self.detectionCon, self.trackCon) # creating the pose object
+        self.mpDraw = mp.solutions.drawing_utils #This is built drawing function to draw dots/connections of the tracked hand landmarks (21 dots per hand)
+
+    def findPose(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) #By default, cap.read() ussing cv2.VideoCapture is captureing BGR for color order; converting to RGB
+         
+        #Process the Pose from the image (video capture), the results are tracking the Pose from video capture
+        #Make result a var with in the self, so it can be used again.
+        results = self.pose.process(imgRGB)
+        
+        if results.pose_landmarks: #If hands detected
+            if draw:
+                self.mpDraw.draw_landmarks(img, results.pose_landmarks, self.mpPose.POSE_CONNECTIONS)
+                for id, lm in enumerate(results.pose_landmarks.landmark):
+                    h, w, c = img.shape #Get the height, width of the image capture
+                    cx, cy = int(lm.x*w), int(lm.y*h) #Calculate the normolized position to pixel
+                    cv2.circle(img, (cx,cy), 5, (255,0,0), cv2.FILLED)
+                    print(id, cx, cy)
+        return img
+    def 
+# A main() function that can be run itself.
+def main():
+
+    cap = cv2.VideoCapture('Videos/video2.mp4') #Create the video capture object
+    
+    pTime = 0 #previous time
+    cTime = 0 #current time
+    
+    detector = poseDetector() #Create the detector object based on the class above
+    
+    while True:
+        sucess, img = cap.read()
+
+        img = detector.findPose(img)
+        cTime = time.time()
+
+        fps = 1/(cTime-pTime)
+        pTime = cTime
+
+        cv2.putText(img, str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, color=(255,0,255), thickness=3) #Display the FPS on the screen, agruments at https://www.geeksforgeeks.org/python-opencv-cv2-puttext-method/
+
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
+
+if __name__ == "__main__":
+    main()
